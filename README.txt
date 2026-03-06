dbea4abd3a145306d520c6fcfab042349b2ff468
Josh Sterling <joshuamaxsterling@gmail.com>
2026-03-06 17:01:26 +0000

    Added leaderboard system and cleaned up UX. Flowchart and README updated

--- README.txt
+++ README.txt
@@ -19,25 +19,29 @@ How to run the game:
 1. Download or clone the project files.
 2. Open terminal and navigate to the project folder.
 3. Run the script using: python guess_the_number.py
+4. The game will guide you through what to do
+
+NOTE: The secret number is within the range 1-100
 
 ----------------------------------------------------------------------------------------------------------
 
 
 Features:
 
+- Player name input
 - Random number generation
 - Input validation using try/except
 - Attempt limit
 - Score based on remaining attempts
 - Option to play again
+- Option to play as new player
+- Leaderboard tracking total score, games played and fewest guesses per player
 
 ----------------------------------------------------------------------------------------------------------
 
 
 Future Improvements
 
-- Total score accumulation
-- Additional player statistics
-- Leaderboard
+- Allow the leaderboard to be saved when script ends and reloaded in when script restarts
 
 
--- guess_the_number.py
+++ guess_the_number.py
@@ -1,50 +1,102 @@
 import random
-play_again = "y"
+
+leaderboard = {}
 
 def check_guess(secret_number): 
-    '''Loop to guess input and check against secret number'''        
-    try:
-        guess = int(input("Guess the Secret Number: "))
+        '''Loop to guess input and check against secret number'''        
+        try:
+            guess = int(input("Guess the Secret Number: "))
 
-        if guess == secret_number:
-            print("Correct")
-            return True, True
-        elif guess < secret_number:
-            print("Higher")
-            return False, True
-        else:
-            print("Lower")
-            return False, True   
-    except ValueError:
-        print("Please enter Valid Number")
-        return False, False
-
-while True:
-
-    secret_number = random.randint(1,100) #Generate number between 1-100
-    attempts = 10 #Number of attempts left
-    print("You have %d Attempts left" %attempts)
-
-    while attempts > 0:
+            if guess == secret_number:
+                print("Correct!")
+                return True, True
+            elif guess < secret_number:
+                print("The Secret Number is Higher")
+                return False, True
+            else:
+                print("The Secret Number is Lower")
+                return False, True   
+        except ValueError:
+            print("Please enter Valid Number")
+            return False, False
         
-        correct, valid = check_guess(secret_number)
+def display_leaderboard(leaderboard):
+    '''Displays the current leaderboard ordered by descending total points'''
+    print("\nLEADERBOARD\n")
 
-        if correct == True:
-            score = attempts*10
-            print("Score = %d" %score)
+    for player in sorted(leaderboard, key=lambda player_sort: leaderboard[player_sort]["total_score"], reverse=True):
+
+        print("%s: \n Total Score: %d | Fewest Guesses: %s | Games Played: %d\n" %
+            (player,
+            leaderboard[player]["total_score"],
+            leaderboard[player]["fewest_guesses"],
+            leaderboard[player]["games_played"]))
+        
+while True:
+    player_name = input("Enter Name: ")
+
+    if player_name not in leaderboard:
+        leaderboard[player_name] = {
+        "total_score": 0,
+        "games_played": 0,
+        "fewest_guesses": None
+    }
+        print("Welcome New Player - %s" %player_name)
+    else:
+        print("Welcome back, %s" %player_name)
+
+    while True:
+
+        secret_number = random.randint(1,100) #Generate number between 1-100
+        attempts = 10 #Number of attempts left
+        print("\nYou have %d Attempts left" %attempts)
+
+        won = False
+
+        while attempts > 0:
+            
+            correct, valid = check_guess(secret_number)
+
+            if correct == True:
+                won = True
+                score = attempts*10
+                print("Score = %d" %score)
+                break
+
+            if valid == True:
+                attempts -= 1 
+                print("\nYou have %d Attempts left" %attempts)
+        if attempts == 0:
+            score = 0
+            print("You have run out of attempts! Score = 0. \nThe number was %d" %secret_number)    
+        
+        guesses_used = 11 - attempts
+
+        leaderboard[player_name]["games_played"] += 1
+        leaderboard[player_name]["total_score"] += score
+        if won == True:
+            if leaderboard[player_name]["fewest_guesses"] is None or guesses_used < leaderboard[player_name]["fewest_guesses"]:
+                leaderboard[player_name]["fewest_guesses"] = guesses_used
+
+
+        play_again = input("Play Again? (y/n): ").lower()
+        while play_again != "y" and play_again != "n":
+            print("Please Enter y or n!")
+            play_again = input("Play Again? (y/n): ").lower()
+
+        if play_again == "n":
+            print("Thanks for Playing %s!" %player_name)
             break
 
-        if valid == True:
-            attempts -= 1 
-            print("You have %d Attempts left" %attempts)
-    if attempts == 0:
-        print("You have run out of attempts! Score = 0. \nThe number was %d" %secret_number)    
-
-    play_again = input("Play Again? (y/n): ").lower()
-    while play_again != "y" and play_again != "n":
+    display_leaderboard(leaderboard)
+        
+    new_player = input("New Player? (y/n): ").lower()
+        
+    while new_player != "y" and new_player != "n":
         print("Please Enter y or n!")
-        play_again = input("Play Again? (y/n): ").lower()
+        new_player = input("New Player? (y/n): ").lower()
 
-    if play_again == "n":
+    if new_player == "n":
         print("Thanks for Playing!")
+        print("Note: Leaderboard Cleared")
         break
